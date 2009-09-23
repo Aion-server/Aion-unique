f1894c415e8e12b9a7130a48b03b748f4ba94317
alexa026 <alexa026@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-09-23 14:28:14 +0000

    New login and game server console

--- AE-go_GameServer/dist/StartGS.bat
+++ AE-go_GameServer/dist/StartGS.bat
@@ -1,3 +1,25 @@
 @echo off
-TITLE Aion-Unique - Game Server
+title Aion-Unique GameServer Console
+:start
+echo Starting Aion-Unique Game Server.
+echo.
+REM -------------------------------------
+REM Default parameters for a basic server.
 java -Xms128m -Xmx1024m -ea -Xbootclasspath/p:./libs/jsr166.jar -javaagent:libs/ae_commons.jar -cp ./libs/*;ae_gameserver.jar com.aionemu.gameserver.GameServer
+REM
+REM -------------------------------------
+
+SET CLASSPATH=%OLDCLASSPATH%
+
+
+if ERRORLEVEL 1 goto error
+goto end
+:error
+echo.
+echo GameServer Terminated Abnormaly, Please Verify Your Files.
+echo.
+:end
+echo.
+echo GameServer Terminated.
+echo.
+pause
--- AE-go_LoginServer/dist/StartLS.bat
+++ AE-go_LoginServer/dist/StartLS.bat
@@ -1,3 +1,25 @@
 @echo off
-TITLE Aion-Unique - Login Server
+title Aion-Unique Login Server Console
+:start
+echo Starting Aion-Unique Login Server.
+echo.
+REM -------------------------------------
+REM Default parameters for a basic server.
 java -Xms8m -Xmx32m -ea -Xbootclasspath/p:./libs/jsr166.jar -javaagent:libs/ae_commons.jar -cp ./libs/*;ae_login.jar com.aionemu.loginserver.LoginServer
+REM
+REM -------------------------------------
+
+SET CLASSPATH=%OLDCLASSPATH%
+
+
+if ERRORLEVEL 1 goto error
+goto end
+:error
+echo.
+echo GameServer Terminated Abnormaly, Please Verify Your Files.
+echo.
+:end
+echo.
+echo GameServer Terminated.
+echo.
+pause
