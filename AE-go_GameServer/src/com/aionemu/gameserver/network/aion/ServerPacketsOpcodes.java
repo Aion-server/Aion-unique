0eca6895c0bf29ecad072b7b1b0dd8bc8f677718
alexa026 <alexa026@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-09-26 11:51:53 +0000

    hp update stat is now seted to -1 becouse that 1 is for 1.2, and 0x12 is now opcode for inventory update.

--- AE-go_GameServer/src/com/aionemu/gameserver/network/aion/ServerPacketsOpcodes.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/network/aion/ServerPacketsOpcodes.java
@@ -65,7 +65,7 @@ public class ServerPacketsOpcodes
 		addPacketOpcode(SM_VERSION_CHECK.class, Version.Chiness ? 0xF6 : 0xF9, idSet);
 		addPacketOpcode(SM_SYSTEM_MESSAGE.class, Version.Chiness ? 0x2A : 0x30, idSet);
 		addPacketOpcode(SM_STATUPDATE_MP.class, Version.Chiness ? -1 : 0x14, idSet);
-		addPacketOpcode(SM_STATUPDATE_HP.class, Version.Chiness ? -1 : 0x12, idSet);
+		addPacketOpcode(SM_STATUPDATE_HP.class, Version.Chiness ? -1 : -1, idSet); //0x12//need opcode for 1.5.x client
 		addPacketOpcode(SM_STATUPDATE_EXP.class, Version.Chiness ? -1 : 0x01, idSet); // 1.5.x
 		addPacketOpcode(SM_STATUPDATE_DP.class, Version.Chiness ? -1 : 0x21, idSet); // 0x17
 		addPacketOpcode(SM_STATS_INFO.class, Version.Chiness ? 0x29 : 0xF8, idSet);
@@ -107,7 +107,7 @@ public class ServerPacketsOpcodes
 		
 		addPacketOpcode(SM_CASTSPELL.class, Version.Chiness ? -1 : 0x18, idSet);	//1.5.x 
 		addPacketOpcode(SM_CASTSPELL_END.class, Version.Chiness ? -1 : 0x42, idSet);	//1.5.x 
-		addPacketOpcode(SM_INVENTORY_UPDATE.class, Version.Chiness ? -1 : -1, idSet);	//not done...
+		addPacketOpcode(SM_INVENTORY_UPDATE.class, Version.Chiness ? -1 : 0x12, idSet);	//1.5.x 
 		//Unrecognized Opcodes:
 		
 		//addPacketOpcode(SM_TELEPORT.class, Version.Chiness ? -1 : 0xBD, idSet);
