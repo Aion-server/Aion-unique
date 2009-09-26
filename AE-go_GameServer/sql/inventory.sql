ad0023ef2546e2cba9bbf788a73d8307bbe4c097
avol <avol@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-09-26 18:18:51 +0000

    Inventory Part1: sql table for items.

---
+++ AE-go_GameServer/sql/inventory.sql
@@ -0,0 +1,8 @@
+ CREATE TABLE `inventory` (
+`itemUniqueId` BIGINT NOT NULL AUTO_INCREMENT ,
+`itemId` INT NOT NULL DEFAULT '0',
+`itemNameId` INT NOT NULL DEFAULT '0',
+`itemCount` INT NOT NULL DEFAULT '0',
+`itemOwner` INT NOT NULL DEFAULT '0',
+PRIMARY KEY ( `itemUniqueId` )
+) ENGINE = MYISAM 
