3b1e9466b3316aef9c3557ed8f2653635fb889a4
ergoz <ergoz@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-09-28 15:31:16 +0000

    Critical fixes in server installer tool

--- tools/au_server_installer/Install.bat
+++ tools/au_server_installer/Install.bat
@@ -32,8 +32,8 @@ set fastend=no
 
 set user=root
 set pass=1q2w3e4r
-set LSDBname=au_server_ls_test
-set GSDBname=au_server_gs_test
+set LSDBname=au_server_ls_tt
+set GSDBname=au_server_gs_tt
 set DBHost=localhost
 
 REM ========================================= :create databases
@@ -58,7 +58,7 @@ echo.(3) = Backup DB
 echo.
 echo.(4) = Restore DB
 echo.
-echo.(e) = Quit
+echo.(q) = Quit
 echo.
 set button=x
 set /p button=What do you want ?:
@@ -66,7 +66,7 @@ if /i %button%==1 goto Install_Login_menu
 if /i %button%==2 goto Install_Game_menu
 if /i %button%==3 goto Backup_menu
 if /i %button%==4 goto Restore_menu
-if /i %button%==e goto Exit
+if /i %button%==q goto Exit
 goto Main_menu
 
 REM ========================================= :Install_Login_menu
@@ -77,17 +77,18 @@ echo.
 echo.--- Install Login Server.. ---
 echo.
 echo.
-echo.(i) = Install Login Server. Warning !!! accounts, gameservers, banned_ips will be deleted!!!
+echo.(i) = Install Login Server. Account_data, account_time, 
+echo.gameservers, banned_ip,  will be deleted!!!
 echo.
 echo.(m) = Main menu
 echo.
-echo.(e) = Quit
+echo.(q) = Quit
 echo.
 set button=x
 set /p button=Your choice ?:
 if /i %button%==i goto Install_Login 
 if /i %button%==m goto Main_menu
-if /i %button%==e goto Exit
+if /i %button%==q goto Exit
 goto Install_Login_menu
 
 REM ========================================= :Install_Game_menu
@@ -95,20 +96,20 @@ REM ========================================= :Install_Game_menu
 cls
 echo.Install Aion Unique and ErgoZ Developers Team DB
 echo.
-echo.Install Game Server & Updates
+echo.Install Game Server and Updates
 echo.
 echo.
-echo.(i) = Install Game Server. Warning !!! All Game Server Database will be deleted !!!
+echo.(i) = Install Game Server. All Game Server Database will be deleted !!!
 echo.
 echo.(m) = Main menu
 echo.
-echo.(e) = Quit
+echo.(q) = Quit
 echo.
 set button=x
 set /p button=Your choice ?:
 if /i %button%==i goto Install_Game 
 if /i %button%==m goto Main_menu
-if /i %button%==exit goto Exit
+if /i %button%==q goto Exit
 goto Install_Game_menu
 
 REM ========================================= :Backup_menu
@@ -123,13 +124,13 @@ echo.(1) = Full backup
 echo.
 echo.(m) = Main menu
 echo.
-echo.(e) = Quit
+echo.(q) = Quit
 echo.
 set button=x
 set /p button=Select backup type ?:
 if /i %button%==1 goto full_backup
 if /i %button%==m goto Main_menu
-if /i %button%==e goto Exit
+if /i %button%==q goto Exit
 goto Backup_menu
 
 REM ========================================= :Restore_menu
@@ -148,12 +149,12 @@ echo.Enter a full filename do you want to restore to the database !
 echo.
 echo.(m) = Main menu
 echo.
-echo.(e) = Quit
+echo.(q) = Quit
 echo.
 set filename=x
 set /p filename=Enter filename ?:
 if /i %filename%==m goto Main_menu
-if /i %filename%==e goto Exit
+if /i %filename%==q goto Exit
 if /i %filename%==%filename% goto restore_DB
 goto Restore_menu
 
@@ -166,7 +167,16 @@ set ctime=%ctime%'%TIME:~3,2%'%TIME:~6,2%
 echo.
 echo Making a full backup into %LSDBname%-%DATE%-%ctime%_backup_full.sql
 echo.
-mysqldump.exe --add-drop-table -h %DBHost% -u %user% --password=%pass% %DBname% > backup/%LSDBname%-%DATE%-%ctime%_backup_full.sql
+mysqldump.exe  --add-drop-table -h %DBHost% -u %user% --password=%pass% %LSDBname% > backup/%LSDBname%-%DATE%-%ctime%_backup_full.sql
+echo.
+echo.[Drop Table]: account_data
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% --execute="DROP TABLE IF EXISTS account_data"
+echo.[Drop Table]: account_time
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% --execute="DROP TABLE IF EXISTS account_time"
+echo.[Drop Table]: banned_ip
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% --execute="DROP TABLE IF EXISTS banned_ip"
+echo.[Drop Table]: gameservers
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% --execute="DROP TABLE IF EXISTS gameservers"
 echo.
 echo.Installing Login Server !!!
 echo.
@@ -182,7 +192,7 @@ mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% < login/banned_i
 echo.[Installing Table]: gameservers.sql;
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% < login/gameservers.sql
 echo.
-echo.Login Server Installed !!!
+echo.Login Server Installed !!!  Now you are going to main menu.
 echo.
 pause
 goto main_menu
@@ -197,37 +207,47 @@ echo Making a full backup into %GSDBname%-%DATE%-%ctime%_backup_full.sql
 echo.
 mysqldump.exe  --add-drop-table -h %DBHost% -u %user% --password=%pass% %GSDBname% > backup/%GSDBname%-%DATE%-%ctime%_backup_full.sql
 echo.
+echo.[Drop Table]: blocks
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS blocks"
+echo.[Drop Table]: friends
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS friends"
+echo.[Drop Table]: inventory
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS inventory"
+echo.[Drop Table]: player_appearance
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS player_appearance"
+echo.[Drop Table]: player_macrosses
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS player_macrosses"
+echo.[Drop Table]: player_skills
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS player_skills"
+echo.[Drop Table]: players
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS players"
+echo.[Drop Table]: server_variables
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS server_variables"
+echo.[Drop Table]: skill_trees
+mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% --execute="DROP TABLE IF EXISTS skill_trees"
+echo.
 echo.Installing general tables !!!
 echo.
-echo Loading Table: blocks.sql
+echo [Installing Table]:  blocks.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/blocks.sql
-
 echo [Installing Table]: friends.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/friends.sql
-
 echo [Installing Table]: inventory.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/inventory.sql
-
 echo [Installing Table]: player_appearance.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/player_appearance.sql
-
 echo [Installing Table]: player_macrosses.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/player_macrosses.sql
-
 echo [Installing Table]: player_skills.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/player_skills.sql
-
 echo [Installing Table]: players.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/players.sql
-
 echo [Installing Table]: server_variables.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/server_variables.sql
-
 echo [Installing Table]: skill_trees.sql
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %GSDBname% < game/skill_trees.sql
-
 echo.
-echo.Complete !!!
+echo.Game Server Installed !!!  Now you are going to main menu.
 echo.
 if /I %fastend%==yes goto :EOF
 pause
@@ -283,13 +303,21 @@ pause
 mysql.exe -h %DBHost% -u %user% --password=%pass% -D %LSDBname% < backup/%filename%
 goto Main_menu
 
-REM ========================================= :not_working_now
-:not_working_now
-echo.
-echo Not working NOW  !!!
-echo.
-pause
-goto Main_menu
-
 REM ========================================= :end
 :Exit
+call :colors 03
+title Aion Unique installer - Script execution finished
+cls
+echo.
+echo Aion Unique installer version 0.0.0.1 beta
+echo (C) 2009-2010 ErgoZ Development Team and Aion Unique Dev Team
+echo Aion Unique installer comes with ABSOLUTELY NO WARRANTY;
+echo This is free software, and you are welcome to redistribute it
+echo under certain conditions; See the file gpl license.txt for further
+echo details.
+echo.
+echo Thanks for using our software.
+echo Visit http://www.aionunique.smfnew.com for more info about
+echo the AION UNIQUE project.
+echo.
+pause
