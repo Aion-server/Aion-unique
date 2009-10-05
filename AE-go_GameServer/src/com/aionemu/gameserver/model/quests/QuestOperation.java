0df664e766b877c81de352ceda0c7c555e3d932e
alexa026 <alexa026@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-10-05 10:43:21 +0000

    Quests engine part 1...

---
+++ AE-go_GameServer/src/com/aionemu/gameserver/model/quests/QuestOperation.java
@@ -0,0 +1,20 @@
+package com.aionemu.gameserver.model.quests;
+
+import org.w3c.dom.Node;
+
+public abstract class QuestOperation
+{
+
+       protected QuestOperation(Node node)
+       {
+       }
+
+       public void operate(QuestState state) throws QuestEngineException
+       {
+               doOperate(state);
+       }
+
+       protected abstract void doOperate(QuestState state) throws QuestEngineException;
+
+       public abstract String getName();
+}
