be9c4bc0a8c6668fa3268be9b5347bcb4f245859
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-10-08 12:12:22 +0000

    SM_SELL_LIST small refactoring.

--- AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_SELL_LIST.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_SELL_LIST.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of aion-unique <aionunique.smfnew.com>.
+ *
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-unique is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.aionemu.gameserver.network.aion.serverpackets;
 
 import java.nio.ByteBuffer;
@@ -7,21 +23,21 @@ import com.aionemu.gameserver.network.aion.AionServerPacket;
 import com.aionemu.gameserver.model.gameobjects.Npc;
 
 /**
- * Author: Blackmouse
+ * @author Blackmouse
  */
 public class SM_SELL_LIST extends AionServerPacket
 {
-	private Npc _merchant;
+	private int merchantId;
 
 	public SM_SELL_LIST(Npc npc)
 	{
-		_merchant = npc;
+		merchantId = npc.getObjectId();
 	}
 
 	@Override
 	public void writeImpl(AionConnection con, ByteBuffer buf)
 	{
-		writeD(buf, _merchant.getObjectId());
+		writeD(buf, merchantId);
 		writeD(buf, 20/**_merchant.getPercentToSell()*/);
 	}
 }
