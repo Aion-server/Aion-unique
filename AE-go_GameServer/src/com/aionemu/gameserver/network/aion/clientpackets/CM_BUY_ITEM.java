36ecbd5cbe58dacffbf60b07c86a5deb7ff66ece
orz <orz@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-10-21 07:34:52 +0000

    Buy item remove Kinah from inventory Sell item to shop. Shop Completed. Thx sarrek

--- AE-go_GameServer/src/com/aionemu/gameserver/network/aion/clientpackets/CM_BUY_ITEM.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/network/aion/clientpackets/CM_BUY_ITEM.java
@@ -28,8 +28,11 @@ import com.aionemu.gameserver.model.gameobjects.player.Inventory;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
 import com.aionemu.gameserver.model.templates.ItemTemplate;
 import com.aionemu.gameserver.network.aion.AionClientPacket;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DELETE_ITEM;
 import com.aionemu.gameserver.network.aion.serverpackets.SM_INVENTORY_INFO;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_UPDATE_ITEM;
 import com.aionemu.gameserver.services.ItemService;
+import com.aionemu.gameserver.utils.PacketSendUtility;
 import com.google.inject.Inject;
 
 /**
@@ -39,11 +42,11 @@ import com.google.inject.Inject;
  */
 public class CM_BUY_ITEM extends AionClientPacket
 {
-	public int npcObjId;
-	public int unk1; 
-	public int amount;
-	public int itemId;
-	public int count;
+	private int npcObjId;
+	private int unk1; 
+	private int amount;
+	private int itemId;
+	private int count;
 
 	public int unk2;
 	
@@ -70,42 +73,48 @@ public class CM_BUY_ITEM extends AionClientPacket
 		npcObjId = readD();
 		unk1	 = readH();
 		amount = readH(); //total no of items
+		int totalprice = 0;
+		
 		Player player = getConnection().getActivePlayer();
 		Inventory bag = player.getInventory();
-				
-		if ((unk1 != 1)||(unk1 != 12))
-			log.info(String.format("Unhandle shop action unk1: %d", unk1));
 		
 		for (int i = 0; i < amount; i++) 
 		{
 			itemId = readD();
 			count  = readD();
 			unk2   = readD();
-			
-			//Todo check the amount of kinah at server side
-			
+							
 			if (unk1 == 12) //buy
 			{
 				Item item = itemService.newItem(itemId, count);
-				Item resultItem = bag.addToBag(item);
-				if(resultItem != null)
+				if((item != null)&&(!bag.isFull()))
 				{
+					Item resultItem = bag.addToBag(item);
+					totalprice = totalprice - item.getItemTemplate().getPrice() * count;
 					//TODO check retail the real packets that sent to player
 					sendPacket(new SM_INVENTORY_INFO(Collections.singletonList(resultItem)));
 				}
-		
 			}
 			else if (unk1 == 1) //sell
 			{
-				log.info(String.format("Sell itemId: %d count: %d", itemId, count));
+				Item item = bag.getItemByObjId(itemId);
+				if (item != null)
+				{
+					bag.removeFromBag(item);
+					totalprice = totalprice + item.getItemTemplate().getPrice()* count / 4;
+				//TODO check retail the real packets that sent to player
+				sendPacket(new SM_DELETE_ITEM(item.getObjectId()));
+				}
 			}
 			else
 			{
-				log.info(String.format("Unhandle shop action: %d", unk1));
-				log.info(String.format("ItemId: %d count: %d", itemId, count));
+				log.info(String.format("Unhandle shop action unk1: %d", unk1));
 			}
 		}
 
+		bag.getKinahItem().increaseItemCount(2* totalprice);
+		if (totalprice != 0)
+			PacketSendUtility.sendPacket(player, new SM_UPDATE_ITEM(bag.getKinahItem()));
 		//TODO uncomment only after inventory slot save testing
 		//DAOManager.getDAO(InventoryDAO.class).store(bag);
 
@@ -118,4 +127,5 @@ public class CM_BUY_ITEM extends AionClientPacket
 	protected void runImpl()
 	{
 	}
+
 }
