ec1caf786fd1448588874dab33be0fafce547a8b
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-10-28 13:21:11 +0000

    datapack was useless thing)

--- AE-go_DataPack/sql/gameserver/player_skills.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `player_skills`;
-CREATE TABLE `player_skills` (
-  `player_id` int(11) NOT NULL default '0',
-  `skillId` int(11) NOT NULL default '0',
-  `skillLevel` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`player_id`,`skillId`,`skillLevel`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
