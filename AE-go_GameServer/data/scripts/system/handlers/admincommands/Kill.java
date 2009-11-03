3464538e3b684cb81d6c5105adbfc50b9897394b
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-11-03 14:27:23 +0000

    Kill admin command. Don't try to kill yourself)

---
+++ AE-go_GameServer/data/scripts/system/handlers/admincommands/Kill.java
@@ -0,0 +1,53 @@
+package admincommands;
+
+import java.util.Collections;
+
+import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.gameobjects.player.Inventory;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_INVENTORY_INFO;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.chathandlers.AdminCommand;
+
+/*
+ * This file is part of aion-unique <aion-unique.com>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @author ATracer
+ *
+ */
+public class Kill extends AdminCommand
+{
+	public Kill()
+	{
+		super("kill");
+	}
+	
+	@Override
+	public void executeCommand(Player admin, String[] params)
+	{
+		Creature creature = admin.getTarget();
+		if(creature == null)
+		{
+			PacketSendUtility.sendMessage(admin, "No target selected");
+			return;
+		}
+		creature.getController().onAttack(admin);
+		creature.getLifeStats().reduceHp(1000000); //hope it is enough to kill every life :)	
+	}
+}
