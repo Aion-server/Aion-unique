f1f07c528be8e7104ab39fa5ae5c01bcaa3d4fb4
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2009-12-24 11:48:17 +0000

    Sell item patch (allow to sell part of item stack), thx kosyachok

--- AE-go_GameServer/src/com/aionemu/gameserver/services/TradeService.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/services/TradeService.java
@@ -38,7 +38,7 @@ public class TradeService
 {
 	@Inject
 	private ItemService itemService;
-	
+
 	/**
 	 * 
 	 * @param player
@@ -49,24 +49,24 @@ public class TradeService
 	{
 		Inventory inventory  = player.getInventory();
 		Item kinahItem = inventory.getKinahItem();
-		
+
 		int freeSlots = inventory.getLimit() - inventory.getUnquippedItems().size() + 1;
 		//1. check kinah
 		int kinahCount = kinahItem.getItemCount();
 		int tradeListPrice = tradeList.calculateBuyListPrice() * 2;
-		
+
 		if(kinahCount < tradeListPrice)
 			return false; //ban :)
-		
+
 		//2. check free slots, need to check retail behaviour
 		if(freeSlots < tradeList.size())
 			return false; //TODO message
-		
+
 		List<Item> addedItems = new ArrayList<Item>();
 		for(TradeItem tradeItem : tradeList.getTradeItems())
 		{
 			Item item = itemService.newItem(tradeItem.getItemId(), tradeItem.getCount());
-			
+
 			if(item != null)
 			{
 				Item resultItem = inventory.addToBag(item);
@@ -89,7 +89,7 @@ public class TradeService
 		//TODO message
 		return true;
 	}
-	
+
 	/**
 	 * 
 	 * @param player
@@ -99,7 +99,7 @@ public class TradeService
 	public boolean performSellToShop(Player player, TradeList tradeList)
 	{
 		Inventory inventory = player.getInventory();
-		
+
 		int kinahReward = 0;		
 		for(TradeItem tradeItem : tradeList.getTradeItems())
 		{
@@ -107,18 +107,31 @@ public class TradeService
 			// 1) don't allow to sell fake items;
 			if(item == null)
 				return false;
-			
-			inventory.removeFromBag(item);
-			
-			kinahReward += item.getItemTemplate().getPrice() * item.getItemCount();
-			//TODO check retail packet here
-			PacketSendUtility.sendPacket(player, new SM_DELETE_ITEM(item.getObjectId()));
+
+			if(item.getItemCount() - tradeItem.getCount() == 0)
+			{
+				inventory.removeFromBag(item);    // need to be here to avoid exploit by sending packet with many items with same unique ids
+				kinahReward += item.getItemTemplate().getPrice() * item.getItemCount();
+				//TODO check retail packet here
+				PacketSendUtility.sendPacket(player, new SM_DELETE_ITEM(item.getObjectId()));
+			}
+			else if(item.getItemCount() - tradeItem.getCount() > 0)
+			{
+				if(item.decreaseItemCount(tradeItem.getCount()))
+				{
+					//TODO check retail packet here
+					kinahReward += item.getItemTemplate().getPrice() * tradeItem.getCount();
+					PacketSendUtility.sendPacket(player, new SM_UPDATE_ITEM(item));
+				}
+				else return false;
+			}
+			else return false;
 		}
-		
+
 		Item kinahItem = inventory.getKinahItem();
 		kinahItem.increaseItemCount(kinahReward / 2);
 		PacketSendUtility.sendPacket(player, new SM_UPDATE_ITEM(kinahItem));
-		
+
 		return true;
 	}
 }
