449398d919b112d3e8f2d36f3862d728a8563e69
StasON <StasON@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-09 20:41:17 +0000

    Quests: Add quest 1182 Ancient Fragment Stone. Thx Nephis

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/verteron/_1182AncientStoneFragment.java
@@ -0,0 +1,110 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.verteron;
+
+import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
+import com.aionemu.gameserver.questEngine.QuestEngine;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
+
+/**
+ * @author MrPoke remod by Nephis
+ * 
+ */
+public class _1182AncientStoneFragment extends QuestHandler
+{
+	private final static int	questId	= 1182;
+
+	public _1182AncientStoneFragment()
+	{
+		super(questId);
+		QuestEngine.getInstance().setNpcQuestData(203099).addOnTalkEvent(questId);
+		QuestEngine.getInstance().setQuestItemIds(182200549).add(questId);
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		if(targetId == 0)
+		{
+			if(env.getDialogId() == 1002)
+			{
+				QuestEngine.getInstance().getQuest(env).startQuest(QuestStatus.START);
+				PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(0, 0));
+				return true;
+			}
+		}
+		else if(targetId == 203099)
+		{
+			if(qs != null)
+			{
+				if(env.getDialogId() == 25 && qs.getStatus() == QuestStatus.START)
+				{
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2375);
+				}
+				else if(env.getDialogId() == 1009)
+				{
+					player.getInventory().removeFromBagByItemId(182200549, 1);
+					qs.getQuestVars().setQuestVar(1);
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(player, qs);
+					return defaultQuestEndDialog(env);
+				}
+				else
+					return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onItemUseEvent(QuestEnv env, Item item)
+	{
+		final Player player = env.getPlayer();
+		final int id = item.getItemTemplate().getItemId();
+		final int itemObjId = item.getObjectId();
+
+		if(id != 182200549)
+			return false;
+		PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId, id,
+			3000, 0, 0), true);
+		ThreadPoolManager.getInstance().schedule(new Runnable(){
+			@Override
+			public void run()
+			{
+				PacketSendUtility.broadcastPacket(player, new SM_ITEM_USAGE_ANIMATION(player.getObjectId(), itemObjId,
+					id, 0, 1, 0), true);
+				sendQuestDialog(player, 0, 4);
+			}
+		}, 3000);
+		return true;
+	}
+}
