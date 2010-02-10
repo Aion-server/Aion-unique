5afda0e6d1190604c660706b44babda09bd855c5
StasON <StasON@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-10 16:33:02 +0000

    Quests: Add 1013 Hunting Lepharist Revolutionaries thx Mr.Poke and dune11.

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/verteron/_1013HuntingLepharistRevolutionaries.java
@@ -0,0 +1,144 @@
+/*
+ * This file is part of aion-emu <aion-emu.com>.
+ *
+ *  aion-emu is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-emu is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-emu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.verteron;
+
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.questEngine.QuestEngine;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+
+/**
+* @author MrPoke
+*
+*/
+public class _1013HuntingLepharistRevolutionaries extends QuestHandler
+{
+
+   private final static int   questId   = 1013;
+   private final static int[]   mob_ids   = {210688, 210316};
+   
+   public _1013HuntingLepharistRevolutionaries()
+   {
+      super(questId);
+      QuestEngine.getInstance().setNpcQuestData(203126).addOnTalkEvent(questId);
+      QuestEngine.getInstance().addQuestLvlUp(questId);
+      for (int mob_id : mob_ids)
+         QuestEngine.getInstance().setNpcQuestData(mob_id).addOnKillEvent(questId);
+   }
+
+   @Override
+   public boolean onLvlUpEvent(QuestEnv env)
+   {
+      Player player = env.getPlayer();
+      QuestState qs = player.getQuestStateList().getQuestState(questId);
+      if(qs == null || player.getCommonData().getLevel() < 10 || qs.getStatus() != QuestStatus.LOCKED)
+         return false;
+      qs.setStatus(QuestStatus.START);
+      updateQuestStatus(player, qs);
+      return true;
+   }
+   
+   @Override
+   public boolean onDialogEvent(QuestEnv env)
+   {
+      Player player = env.getPlayer();
+      QuestState qs = player.getQuestStateList().getQuestState(questId);
+      if(qs == null)
+         return false;
+
+      int var = qs.getQuestVars().getQuestVarById(0);
+      int targetId = 0;
+      if(env.getVisibleObject() instanceof Npc)
+         targetId = ((Npc) env.getVisibleObject()).getNpcId();
+
+      if(qs.getStatus() == QuestStatus.START)
+      {
+         if(targetId == 203126)
+         {
+            switch(env.getDialogId())
+            {
+               case 25:
+                  if (var == 0)
+                     return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+                  else if (var == 11)
+                     return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
+                  else if (var >= 12)
+                     {
+                      qs.setStatus(QuestStatus.REWARD);
+                      updateQuestStatus(player, qs);
+                     return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2375);
+                     }
+               case 10000:
+               case 10001:
+                  if (var == 0 || var == 11)
+                  {
+                     qs.getQuestVars().setQuestVarById(0, var + 1);
+                     updateQuestStatus(player, qs);
+                     PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+                     return true;
+                  }
+            }
+         }
+      }
+      else if (qs.getStatus() == QuestStatus.REWARD)
+      {
+		if(targetId == 203126)
+			return defaultQuestEndDialog(env);
+      }
+      return false;
+   }
+   
+   @Override
+   public boolean onKillEvent(QuestEnv env)
+   {
+      Player player = env.getPlayer();
+      QuestState qs = player.getQuestStateList().getQuestState(questId);
+      if(qs == null)
+         return false;
+
+      int var = qs.getQuestVars().getQuestVarById(0);
+      int targetId = 0;
+      if(env.getVisibleObject() instanceof Npc)
+         targetId = ((Npc) env.getVisibleObject()).getNpcId();
+
+      if(qs.getStatus() != QuestStatus.START)
+         return false;
+      switch (targetId)
+      {
+         case 210688:
+            if (var >=1 && var <=11)
+            {
+               qs.getQuestVars().setQuestVarById(0, var + 1);
+               updateQuestStatus(player, qs);
+               return true;
+            }
+         case 210316:
+            if (var ==12)
+            {
+				qs.setStatus(QuestStatus.REWARD);
+				updateQuestStatus(player, qs);
+               return true;
+            }
+      }
+      return false;
+   }
+}
