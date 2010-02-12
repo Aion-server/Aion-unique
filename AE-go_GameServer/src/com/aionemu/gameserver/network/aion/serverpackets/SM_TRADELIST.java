5e54310ac56c5f4d94869c7d2f0eb205bae93fd5
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-12 23:12:47 +0000

    Updated tradelist packet to show price in abyss points and medals for abyss shops

--- AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_TRADELIST.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_TRADELIST.java
@@ -48,7 +48,8 @@ public class SM_TRADELIST extends AionServerPacket
 		if ((tlist != null)&&(tlist.getNpcId()!=0)&&(tlist.getCount()!=0))
 		{
 			writeD(buf, targetObjectId);
-			writeD(buf, 51201); //unknown for 1.5.x
+			writeH(buf, tlist.isAbyss() ? 2 : 1); //abyss or normal
+			writeH(buf, 0xC8);//unk
 			writeC(buf, 0); // unknown
 			writeH(buf, tlist.getCount()); // unknown
 			for(TradeTab tradeTabl : tlist.getTradeTablist())
