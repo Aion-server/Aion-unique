d24609bb594b3852da554e661fba926ce6e7f7cf
MrPoke <MrPoke@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-16 19:41:38 +0000

    Quest 2001 Thinking Ahead

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/ishalgen/_2001ThinkingAhead.java
@@ -0,0 +1,165 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package quest.ishalgen;
+
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.questEngine.QuestEngine;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+
+public class _2001ThinkingAhead extends QuestHandler
+{
+
+	private final static int	questId	= 2001;
+
+	public _2001ThinkingAhead()
+	{
+		super(questId);
+		QuestEngine.getInstance().addQuestLvlUp(questId);
+		QuestEngine.getInstance().setNpcQuestData(203518).addOnTalkEvent(questId);
+		QuestEngine.getInstance().setNpcQuestData(700093).addOnTalkEvent(questId);
+		QuestEngine.getInstance().setNpcQuestData(210369).addOnKillEvent(questId);
+		QuestEngine.getInstance().setNpcQuestData(210368).addOnKillEvent(questId);
+	}
+
+	@Override
+	public boolean onLvlUpEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null || qs.getStatus() != QuestStatus.LOCKED)
+			return false;
+
+		QuestState qs2 = player.getQuestStateList().getQuestState(2100);
+		if(qs2 == null || qs2.getStatus() != QuestStatus.COMPLITE)
+			return false;
+		qs.setStatus(QuestStatus.START);
+		updateQuestStatus(player, qs);
+		return true;
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+			return false;
+
+		int var = qs.getQuestVars().getQuestVarById(0);
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		if(qs.getStatus() == QuestStatus.START)
+		{
+			if(targetId == 203518)
+			{
+				switch(env.getDialogId())
+				{
+					case 25:
+						if(var == 0)
+							return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+						else if(var == 1)
+							return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
+						else if(var == 2)
+							return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1694);
+						return false;
+					case 10000:
+					case 10002:
+						if(var == 0 || var == 2)
+						{
+							qs.getQuestVars().setQuestVarById(0, var + 1);
+							updateQuestStatus(player, qs);
+							PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+							return true;
+						}
+					case 33:
+						if(var == 1)
+						{
+							int itemCount = player.getInventory().getItemCountByItemId(182203002);
+							if(itemCount >= 4)
+							{
+								player.getInventory().removeFromBagByItemId(182203002, itemCount);
+								qs.getQuestVars().setQuestVarById(0, var + 1);
+								updateQuestStatus(player, qs);
+								return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1694);
+							}
+							else
+								return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1693);
+						}
+				}
+			}
+			else if(targetId == 700093)
+			{
+				if(var == 1 && env.getDialogId() == -1)
+					return true;
+			}
+		}
+		else if(qs.getStatus() == QuestStatus.REWARD)
+		{
+			if(targetId == 203518)
+			{
+				if(env.getDialogId() == -1)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2034);
+				else
+					return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onKillEvent(QuestEnv env)
+	{
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+			return false;
+
+		int var = qs.getQuestVars().getQuestVarById(0);
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+
+		if(qs.getStatus() != QuestStatus.START)
+			return false;
+		switch(targetId)
+		{
+			case 210368:
+			case 210369:
+				if(var >= 3 && var < 8)
+				{
+					qs.getQuestVars().setQuestVarById(0, qs.getQuestVars().getQuestVarById(0) + 1);
+					updateQuestStatus(player, qs);
+					return true;
+				}
+				else if(var == 8)
+				{
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(player, qs);
+					return true;
+				}
+		}
+		return false;
+	}
+}
