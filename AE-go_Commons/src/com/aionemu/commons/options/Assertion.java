89c939b85ed27c627dfd7dcaa1bc23ca03b07581
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-18 19:22:13 +0000

    [commons] Disabling assertions by default so they will not hang Dispatcher.

--- AE-go_Commons/src/com/aionemu/commons/options/Assertion.java
+++ AE-go_Commons/src/com/aionemu/commons/options/Assertion.java
@@ -28,5 +28,5 @@ public final class Assertion
 	/**
 	 * False if assertion at Network code should be removed at compile time. [0 overhead]
 	 */
-	public static final boolean	NetworkAssertion	= true;
+	public static final boolean	NetworkAssertion	= false;
 }
