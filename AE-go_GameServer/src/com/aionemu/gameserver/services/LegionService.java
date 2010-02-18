c888180a3d379f9345fb6e2ca8a57a4e06aca009
Simple2008 <Simple2008@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-18 22:41:50 +0000

    Another one..

--- AE-go_GameServer/src/com/aionemu/gameserver/services/LegionService.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/services/LegionService.java
@@ -1068,7 +1068,7 @@ public class LegionService
 			Player memberPlayer;
 			if((memberPlayer = world.findPlayer(memberObjId)) != null)
 			{
-				final LegionMember legionMemberEx = memberPlayer.getLegionMember();
+				final LegionMemberEx legionMemberEx = memberPlayer.getLegionMember();
 				LegionMemberEx legionMemberEx = new LegionMemberEx(memberObjId, legionMember.getLegion(), legionMember
 					.getRank(), legionMember.getNickname(), legionMember.getSelfIntro(), memberPlayer.getName(),
 					memberPlayer.getPlayerClass(), memberPlayer.getLevel(), memberPlayer.getCommonData()
@@ -1076,7 +1076,7 @@ public class LegionService
 			}
 			else
 			{
-				LegionMemberEx legionMemberEx = getOfflineLegionMember(memberObjId);
+				final LegionMemberEx legionMemberEx = getOfflineLegionMember(memberObjId);
 			}
 			if(isValidLegionMemberEx())
 				legionMembers.add(legionMemberEx);
