d376cf2dfd4b70d31997e4d22946b3e40a2f513e
Simple2008 <Simple2008@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-19 19:44:17 +0000

    Forgot to commit main sql

--- AE-go_GameServer/sql/au_server_gs.sql
+++ AE-go_GameServer/sql/au_server_gs.sql
@@ -273,7 +273,7 @@ CREATE TABLE IF NOT EXISTS `legion_members` (
   `legion_id` int(11) NOT NULL,
   `player_id` int(11) NOT NULL,
   `nickname` varchar(16) NOT NULL default '',  
-  `rank` int(1) NOT NULL default '2', 
+  `rank` enum( 'BRIGADE_GENERAL', 'CENTURION', 'LEGIONARY' ) NOT NULL DEFAULT 'LEGIONARY',
   `selfintro` varchar(25) default '',
   PRIMARY KEY  (`player_id`),
   FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
