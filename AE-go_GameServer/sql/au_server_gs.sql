5eb89ee29221daba3b99bdfaaf484fb2bd62c879
Simple2008 <Simple2008@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-20 01:31:43 +0000

    Forgot to update Main SQL

--- AE-go_GameServer/sql/au_server_gs.sql
+++ AE-go_GameServer/sql/au_server_gs.sql
@@ -252,6 +252,7 @@ CREATE TABLE IF NOT EXISTS `legions` (
   `id` int(11) NOT NULL,
   `name` varchar(16) NOT NULL,
   `level` int(1) NOT NULL DEFAULT '1',
+  `contribution_points` INT NOT NULL DEFAULT '0',
   `legionar_permission2` int(11) NOT NULL DEFAULT '64',
   `centurion_permission1` int(11) NOT NULL DEFAULT '104',
   `centurion_permission2` int(11) NOT NULL DEFAULT '8',
