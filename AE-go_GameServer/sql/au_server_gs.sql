26b779b1ef09123f25d8b9d9048a26f29d0370dc
Simple2008 <Simple2008@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-25 12:33:26 +0000

    Updated main sql file.

--- AE-go_GameServer/sql/au_server_gs.sql
+++ AE-go_GameServer/sql/au_server_gs.sql
@@ -256,7 +256,7 @@ CREATE TABLE IF NOT EXISTS `legions` (
   `legionar_permission2` int(11) NOT NULL DEFAULT '64',
   `centurion_permission1` int(11) NOT NULL DEFAULT '104',
   `centurion_permission2` int(11) NOT NULL DEFAULT '8',
-  `disband_time` timestamp NOT NULL default '0000-00-00 00:00:00',
+  `disband_time` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `name_unique` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
