b62ee2f700e0d2474415a8413f47548a04a98efc
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-02-25 17:01:03 +0000

    Added several dummy skill effects so absence of them will not interfere other effects.

---
+++ AE-go_GameServer/src/com/aionemu/gameserver/skillengine/effect/SlowEffect.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-unique is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aionemu.gameserver.skillengine.effect;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import com.aionemu.gameserver.skillengine.model.Effect;
+
+/**
+ * @author ATracer
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SlowEffect")
+public class SlowEffect extends EffectTemplate
+{
+
+	@Override
+	public void applyEffect(Effect effect)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculate(Effect effect)
+	{
+		effect.increaseSuccessEffect();	
+	}
+
+}
