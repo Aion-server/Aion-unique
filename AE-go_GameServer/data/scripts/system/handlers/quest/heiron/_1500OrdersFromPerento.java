27fce9df531543579dc9012566ad84edc9461f61
lord_rex <lord_rex@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-03-07 14:07:15 +0000

    Fix for java error.

--- AE-go_GameServer/data/scripts/system/handlers/quest/heiron/_1500OrdersFromPerento.java
+++ AE-go_GameServer/data/scripts/system/handlers/quest/heiron/_1500OrdersFromPerento.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
  */
-package quest.eltnen;
+package quest.heiron;
 
 import com.aionemu.gameserver.model.gameobjects.Npc;
 import com.aionemu.gameserver.model.gameobjects.player.Player;
