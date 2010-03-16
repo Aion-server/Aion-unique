2f5f157e7f55a86b40c2c41645b22be666d33f71
alikasm8 <alikasm8@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-03-16 21:30:58 +0000

    _2125TheRobberyPlot dune11

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/ishalgen/_2125TheRobberyPlot.java
@@ -0,0 +1,105 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.ishalgen;
+
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+
+/**
+ * @author MrPoke + Dune11
+ * 
+ */
+public class _2125TheRobberyPlot extends QuestHandler
+{
+	private final static int	questId	= 2125;
+
+	public _2125TheRobberyPlot()
+	{
+		super(questId);
+	}
+
+	@Override
+	public void register()
+	{
+		qe.setNpcQuestData(203540).addOnQuestStart(questId);
+		qe.setNpcQuestData(203540).addOnTalkEvent(questId);
+		qe.setNpcQuestData(203514).addOnTalkEvent(questId);
+		qe.setNpcQuestData(203543).addOnTalkEvent(questId);
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(targetId == 203540)
+		{
+			if(qs == null)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+				else
+					return defaultQuestStartDialog(env);
+			}
+		}
+		else if(targetId == 203514)
+		{
+			if(qs != null && qs.getStatus() == QuestStatus.START && qs.getQuestVarById(0) == 0)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
+				else if(env.getDialogId() == 10000)
+				{
+					qs.setQuestVarById(0, qs.getQuestVarById(0) + 1);
+					updateQuestStatus(player, qs);
+					PacketSendUtility
+						.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+					return true;
+				}
+				else
+					return defaultQuestStartDialog(env);
+			}
+		}
+		else if(targetId == 203543)
+		{
+			if(qs != null)
+			{
+				if(env.getDialogId() == 25 && qs.getStatus() == QuestStatus.START)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2375);
+				else if(env.getDialogId() == 1009)
+				{
+					qs.setQuestVar(2);
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(player, qs);
+					return defaultQuestEndDialog(env);
+				}
+				else
+					return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+}
