2299200a88721cf64a01405eed99d61d670e3e24
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-04-05 19:23:29 +0000

    Added race to npc templates

--- AE-go_GameServer/src/com/aionemu/gameserver/model/Race.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/model/Race.java
@@ -37,7 +37,41 @@ public enum Race
 	/**
 	 * Playable races
 	 */
-	ASMODIANS(1);
+	ASMODIANS(1),
+	
+	/**
+	 * Npc races
+	 */
+	LYCAN(2),
+	CONSTRUCT(3),
+	CARRIER(4),
+	DRAKAN(5),
+	LIZARDMAN(6),
+	TELEPORTER(7),
+	NAGA(8),
+	BROWNIE(9),
+	KRALL(10),
+	SHULACK(11),
+	BARRIER(12),
+	PC_LIGHT_CASTLE_DOOR(13),	
+	PC_DARK_CASTLE_DOOR(14),
+	DRAGON_CASTLE_DOOR(15),
+	GCHIEF_LIGHT(16),
+	GCHIEF_DARK(17),
+	DRAGON(18),
+	OUTSIDER(19),
+	RATMAN(20),
+	DEMIHUMANOID(21),	
+	UNDEAD(22),
+	BEAST(23),
+	MAGICALMONSTER(24),
+	ELEMENTAL(25),
+	
+	/**
+	 * Special races
+	 */
+	NONE(26),
+	PC_ALL(27);
 
 	/**
 	 * id of race
