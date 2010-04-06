2bf9c5dd6541aa2d77afb39c9e51432e25332789
lord_rex <lord_rex@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-04-06 12:08:48 +0000

    Initial work on logging:  * Audit(hack) logger  * Chat logger (thanks Divinity)  * Console Logger (Divinity and my little changes)  * GmAuditLogger (thanks Divinity)  * Item logger

---
+++ AE-go_Commons/src/com/aionemu/commons/log4j/filters/AuditFilter.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-unique is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aionemu.commons.log4j.filters;
+
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * @author lord_rex
+ *
+ */
+public class AuditFilter extends Filter
+{
+	/**
+	 * Decides what to do with logging event.<br>
+	 * This method accepts only log events that contain exceptions.
+	 * 
+	 * @param loggingEvent
+	 *            log event that is going to be filtred.
+	 * @return {@link org.apache.log4j.spi.Filter#ACCEPT} if admin command, {@link org.apache.log4j.spi.Filter#DENY}
+	 *         otherwise
+	 */
+	@Override
+	public int decide(LoggingEvent loggingEvent)
+	{
+		Object message = loggingEvent.getMessage();
+
+		if (((String) message).startsWith("[AUDIT]"))
+		{
+			return ACCEPT;
+		}
+
+		return DENY;
+	}
+}
---
+++ AE-go_Commons/src/com/aionemu/commons/log4j/filters/GmAuditFilter.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of aion-emu <aion-emu.com>.
+ *
+ * aion-emu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-emu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-emu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aionemu.commons.log4j.filters;
+
+import org.apache.log4j.spi.Filter;
+import org.apache.log4j.spi.LoggingEvent;
+
+/**
+ * Log4J filter that looks if there is gm audit present in the logging event and accepts event if present. Otherwise it
+ * blocks filtring.
+ * 
+ * @author Divinity
+ */
+public class GmAuditFilter extends Filter
+{
+	/**
+	 * Decides what to do with logging event.<br>
+	 * This method accepts only log events that contain exceptions.
+	 * 
+	 * @param loggingEvent
+	 *            log event that is going to be filtred.
+	 * @return {@link org.apache.log4j.spi.Filter#ACCEPT} if admin command, {@link org.apache.log4j.spi.Filter#DENY}
+	 *         otherwise
+	 */
+	@Override
+	public int decide(LoggingEvent loggingEvent)
+	{
+		Object message = loggingEvent.getMessage();
+
+		if (((String) message).startsWith("[ADMIN COMMAND]"))
+		{
+			return ACCEPT;
+		}
+
+		return DENY;
+	}
+}
