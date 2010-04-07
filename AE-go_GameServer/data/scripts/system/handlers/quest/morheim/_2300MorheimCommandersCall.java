3d482313cfdb39acb75be7cec8de7900f42850e1
alikasm8 <alikasm8@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-04-07 09:05:15 +0000

    Q1023 ANestofLepharists tested work Q2017 TrespassersattheObservatory tested work Q2019 SecuringtheSupplyRoute tested work Q2020 KeepingtheBlackClawTribeinCheck tested work Q2300 MorheimCommandersCall tested work corekt
    By dune11

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/morheim/_2300MorheimCommandersCall.java
@@ -0,0 +1,114 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.morheim;
+
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.questEngine.QuestEngine;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.world.zone.ZoneName;
+
+/**
+ * @author MrPoke + Dune11
+ * 
+ */
+public class _2300MorheimCommandersCall extends QuestHandler
+{
+
+	private final static int	questId	= 2300;
+
+	public _2300MorheimCommandersCall()
+	{
+		super(questId);
+	}
+
+	@Override
+	public void register()
+	{
+		qe.setNpcQuestData(204301).addOnTalkEvent(questId);
+		qe.setQuestEnterZone(ZoneName.MORHEIM_ICE_FORTRESS_220020000).add(questId);
+	}
+
+	@Override
+	public boolean onLvlUpEvent(QuestEnv env)
+	{
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null || player.getCommonData().getLevel() < 19 || qs.getStatus() != QuestStatus.LOCKED)
+			return false;
+		qs.setStatus(QuestStatus.START);
+		updateQuestStatus(player, qs);
+		return true;
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+			return false;
+
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		if(targetId != 204301)
+			return false;
+		if(qs.getStatus() == QuestStatus.START)
+		{
+			if(env.getDialogId() == 25)
+			{
+				qs.setQuestVar(1);
+				qs.setStatus(QuestStatus.REWARD);
+				updateQuestStatus(player, qs);
+				return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+			}
+			else
+				return defaultQuestStartDialog(env);
+		}
+		else if(qs.getStatus() == QuestStatus.REWARD)
+		{
+			if(env.getDialogId() == 17)
+			{
+				int [] ids = {2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042};
+				for (int id : ids)
+				{
+					questService.startQuest(new QuestEnv(env.getVisibleObject(), env.getPlayer(), id, env.getDialogId()), QuestStatus.LOCKED);
+				}
+			}
+			return defaultQuestEndDialog(env);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean onEnterZoneEvent(QuestEnv env, ZoneName zoneName)
+	{
+		if(zoneName != ZoneName.MORHEIM_ICE_FORTRESS_220020000)
+			return false;
+		final Player player = env.getPlayer();
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs != null)
+			return false;
+		env.setQuestId(questId);
+		questService.startQuest(env, QuestStatus.START);
+		return true;
+	}
+}
