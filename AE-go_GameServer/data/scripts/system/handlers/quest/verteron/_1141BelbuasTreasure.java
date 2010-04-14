fb462eb7f4f42c0985faec7b75a1fe691523014e
stpavel <stpavel@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-04-14 12:01:48 +0000

    Added and fixed few quests (1141,1146,1415,1487,1537,1570,2228,2288,2349)

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/verteron/_1141BelbuasTreasure.java
@@ -0,0 +1,91 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-unique is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.verteron;
+
+import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_ITEM_USAGE_ANIMATION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
+
+/**
+ * @author Mr. Poke + Dune11
+ *
+ */
+public class _1141BelbuasTreasure extends QuestHandler
+{
+	private final static int	questId	= 1141;
+
+	public _1141BelbuasTreasure()
+	{
+		super(questId);
+	}
+
+	@Override
+	public void register()
+	{
+		qe.setNpcQuestData(730001).addOnQuestStart(questId);
+		qe.setNpcQuestData(730001).addOnTalkEvent(questId);
+		qe.setNpcQuestData(700122).addOnTalkEvent(questId);
+	}
+	
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+		{
+			if(targetId == 730001)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+				else
+					return defaultQuestStartDialog(env);
+			}
+		}
+		else if(targetId == 700122)
+		{
+			if(qs != null)
+			{
+				if ((qs.getQuestVarById(0) == 0 || qs.getQuestVarById(0) == 0) && env.getDialogId() == -1)
+				{
+					qs.setQuestVar(2);
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(player, qs);
+					return defaultQuestEndDialog(env);
+				}
+				else
+					PacketSendUtility
+						.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+					return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+}
---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/verteron/_1146DelicateMandrake.java
@@ -0,0 +1,101 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ *  aion-unique is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aion-unique is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.verteron;
+
+import java.util.Collections;
+
+import com.aionemu.gameserver.model.gameobjects.Item;
+import com.aionemu.gameserver.model.gameobjects.Creature;
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.templates.quest.QuestItems;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.services.ItemService;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
+import com.google.inject.Inject;
+
+/**
+ * @author Mr. Poke + Dune11
+ *
+ */
+public class _1146DelicateMandrake extends QuestHandler
+{
+	private final static int	questId	= 1146;
+
+	@Inject
+	ItemService itemService;
+
+	public _1146DelicateMandrake()
+	{
+		super(questId);
+	}
+
+	@Override
+	public void register()
+	{
+		qe.setNpcQuestData(203123).addOnQuestStart(questId);
+		qe.setNpcQuestData(203123).addOnTalkEvent(questId);
+		qe.setNpcQuestData(203139).addOnTalkEvent(questId);
+	}
+	
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+		{
+			if(targetId == 203123)
+			{
+				if(env.getDialogId() == 25)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+					if (itemService.addItems(player, Collections.singletonList(new QuestItems(182200519, 1))))
+					return defaultQuestStartDialog(env);
+			}
+		}
+		else if(targetId == 203139)
+		{
+				int itemCount = player.getInventory().getItemCountByItemId(182200519);
+				if(itemCount >= 1)
+			{
+				if ((qs.getQuestVarById(0) == 0 || qs.getQuestVarById(0) == 0) && env.getDialogId() == -1)
+				{
+					qs.setQuestVar(2);
+					player.getInventory().removeFromBagByItemId(182200011, itemCount);
+					qs.setStatus(QuestStatus.REWARD);
+					updateQuestStatus(player, qs);
+					return defaultQuestEndDialog(env);
+				}
+				else
+					PacketSendUtility
+						.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+					return defaultQuestEndDialog(env);
+			}
+		}
+		return false;
+	}
+}
